5b0b12fe3fcf54f3ea952b27ea4ae4c9fa869c22
Abner Gabriel Miranda <81892160+Abner-Gabriel@users.noreply.github.com>
2021-04-15 13:03:06 -0300

    Multas e brasileiro

---
+++ ex008.js
@@ -0,0 +1,7 @@
+console.log('o console funcionou corretamente')
+var vel = 58
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+    if (vel > 60) { //condiçao simples
+        console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`dirija sempre usando sinto de segurança!`)
